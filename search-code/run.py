059bf4b5adb52e3f5f164fc858d84d4349086538
ZixiongLin <32196374+ZixiongLin@users.noreply.github.com>
2018-05-27 20:29:40 +0100

    Add files via upload

---
+++ search-code/run.py
@@ -0,0 +1,23 @@
+# Search methods
+
+import search
+
+ab = search.GPSProblem('A', 'B', search.romania)
+sb = search.GPSProblem('A', 'V', search.romania)
+
+print (search.breadth_first_graph_search(sb).path())
+print (search.depth_first_graph_search(sb).path())
+#print (search.iterative_deepening_search(ab).path())
+#print (search.depth_limited_search(ab).path())
+
+#print (search.ramification_first_graph_search(ab).path())
+print (search.ramification_first_graph_search(sb).path())
+
+print(search.ramification_substimation(sb).path())
+print(search.ramification_substimation(sb).path())
+
+#print search.astar_search(ab).path()
+
+# Result:
+# [<Node B>, <Node P>, <Node R>, <Node S>, <Node A>] : 101 + 97 + 80 + 140 = 418
+# [<Node B>, <Node F>, <Node S>, <Node A>] : 211 + 99 + 140 = 450
